a3c99ec0868f4c16bf184f6068893cf057d3a7c0
Bill Shelton <b.shelton@f5.com>
2015-05-06 20:10:35 -0700

    logging the html of example.com

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require("express"),
-	http = require("http")
+	http = require("http"),
+	Crawler = require("simplecrawler"),
+	StringDecoder = require("string_decoder").StringDecoder
+
+Crawler.crawl("http://example.com/")
+    .on("fetchcomplete", function(queueItem, responseBuffer, response){
+        console.log("Whole item", queueItem);
+        console.log("RESPONSEBUFFER", responseBuffer);
+        console.log("RESPONSE", response);
+        console.log("Completed fetching resource:", queueItem.url);
+        var decoder = new StringDecoder('utf8');
+        textChunk = decoder.write(responseBuffer);
+        console.log(textChunk)
+    });
 
 var app = express();
 
